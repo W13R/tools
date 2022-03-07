172297c8ab479bd1d9c6e2867815bb8dc7aa8922
W13R <9070224-W13R@users.noreply.gitlab.com>
2022-03-07 16:25:38 +0100

    Improve project download script

--- dltools.sh
+++ dltools.sh
@@ -2,21 +2,29 @@
 
 # Copyright (C) 2022 Julian Müller (W13R)
 # License: MIT
-
 # This is a downloader for this project
 
-
-function dl_git {
-
+# from gitlab
+function dl_git_gitlab {
     git clone https://gitlab.com/W13R/tools.git
-
 }
-
-function dl_http_gitlab {
-
+function dl_wget_tar_gitlab {
     wget https://gitlab.com/W13R/tools/-/archive/main/tools-main.tar && tar -xf tools-main.tar && rm tools-main.tar && mv tools-main tools
-
+}
+function dl_curl_tar_gitlab {
+    curl https://gitlab.com/W13R/tools/-/archive/main/tools-main.tar -L -o tools.tar && tar -xf tools.tar && rm tools.tar && mv tools-main tools
 }
 
+# from github
+function dl_git_github {
+    git clone https://gitlab.com/W13R/tools.git
+}
+function dl_wget_zip_github {
+    wget https://github.com/W13R/tools/archive/refs/heads/main.zip && unzip main.zip && rm main.zip && mv tools-main tools
+}
+function dl_curl_zip_github {
+    curl https://github.com/W13R/tools/archive/refs/heads/main.zip -L -o tools.zip && unzip tools.zip && rm tools.zip && mv tools-main tools
+}
 
-dl_git || dl_http_gitlab
+# do it
+dl_git_gitlab || dl_wget_tar_gitlab || dl_curl_tar_gitlab || dl_git_github || dl_wget_zip_github || dl_curl_zip_github
